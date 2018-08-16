ba244dfcf6bbba444e22e0dc9b6a4a66b037e7ea
Jan Dolezel <dolezel.jan@gmail.com>
2018-08-16 11:37:23 +0200

    Enabling CI integration

--- packages/psql/withDb.spec.js
+++ packages/psql/withDb.spec.js
@@ -14,18 +14,20 @@ describe("wrap", () => {
   });
 
   it("should call fn with non changed args when dbClient is first argument", async () => {
+    const connectDB = sinon.spy();
     const fn = sinon.spy();
     const client = new pg.Client();
     const args = [1, 2, 3];
-    await withDb(fn)(client, ...args);
+    await withDb(connectDB)(fn)(client, ...args);
     expect(fn).to.have.been.called;
     expect(fn).to.have.been.calledWith(client, ...args);
   });
 
   it("should call fn with created dbClient when dbClient is not first argument", async () => {
+    const connectDB = sinon.spy(fn => fn(new pg.Client()));
     const fn = sinon.spy();
     const args = [1, 2, 3];
-    await withDb(fn)(...args);
+    await withDb(connectDB)(fn)(...args);
     expect(fn).to.have.been.called;
     expect(fn.getCall(0).args[0]).to.be.an.instanceof(pg.Client);
     args.forEach((arg, i) => expect(fn.getCall(0).args[i + 1]).to.eql(arg));
