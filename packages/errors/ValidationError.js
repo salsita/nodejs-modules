07ca26141038bf9465ec465b3a2b31db61d85017
Jan Dolezel <dolezel.jan@gmail.com>
2020-11-30 09:16:26 +0100

    Prettiered

--- packages/errors/ValidationError.js
+++ packages/errors/ValidationError.js
@@ -1,9 +1,7 @@
 const HTTPStatus = require("http-status");
 const ClientError = require("./ClientError");
 
-module.exports = class ValidationError extends (
-  ClientError
-) {
+module.exports = class ValidationError extends ClientError {
   constructor(message, status = HTTPStatus.BAD_REQUEST) {
     super(message, status);
   }
